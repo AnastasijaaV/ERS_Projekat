b0d85a61be0d36c92c96b8df48ad627122391aaf
Anastasija Vasiljević <vasiljevic.anastasija.1@gmail.com>
2024-05-18 01:15:29 +0200

    Dodavanje Device i IDevice
    Dodavanje nove klase i interfejsa u okviru Consumer

---
+++ ERS_Projekat/ERS_Projekat/Consumer/IDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat.Consumer
+{
+    public interface IDevice
+    {
+    }
+}

e289257af3d026d0858dd419a324849a8cf9258d
Anastasija Vasiljević <vasiljevic.anastasija.1@gmail.com>
2024-05-25 23:59:04 +0200

    HydroelectricTest

---
+++ ERS_Projekat/ERS_Projekat/Test/HydroelectricTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_Projekat.Test
+{
+    [TestFixture]
+    public class HydroelectricTest
+    {
+        // public PowerPlant.Hydroelectric hydroelectric;
+        public PowerPlant.Hydroelectric? hydroelectric;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            hydroelectric = new PowerPlant.Hydroelectric();
+        }
+
+        [Test]
+        public void OutOfRange()
+        {
+            try
+            {
+                hydroelectric?.UpdateProduction(-1);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains("Procenat proizvodnje hidroelektrane je van opsega!", e.Message);
+                return;
+            }
+            Assert.Fail("Exception nije bacen\n");
+
+        }
+
+        [Test]
+        public void InRange()
+        {
+            try
+            {
+                hydroelectric?.UpdateProduction(25);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains("Procenat proizvodnje hidroelektrane je van opsega!", e.Message);
+                return;
+            }
+            Assert.Pass("Procenat proizvodnje je u opsegu!\n");
+
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            hydroelectric = null;
+        }
+
+
+
+
+    }
+}

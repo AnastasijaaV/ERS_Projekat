0068ef265007ab0f9b9b95d83fbc1bc738b4a9bb
Anastasija Vasiljević <vasiljevic.anastasija.1@gmail.com>
2024-05-26 14:58:04 +0200

    SolarPanelTest

--- ERS_Projekat/ERS_Projekat/obj/Debug/net8.0/ERS_Projekat.AssemblyInfo.cs
+++ ERS_Projekat/ERS_Projekat/obj/Debug/net8.0/ERS_Projekat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERS_Projekat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9d6bd85e038cc9d4693b7fe74ff630c5ec7840")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e289257af3d026d0858dd419a324849a8cf9258d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERS_Projekat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERS_Projekat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

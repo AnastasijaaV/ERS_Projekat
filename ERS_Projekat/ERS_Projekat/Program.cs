a171819bfc53543a61559640c5230815d149969d
Mihailo39 <148571122+Mihailo39@users.noreply.github.com>
2024-05-26 16:26:38 +0200

    Program.cs

--- ERS_Projekat/ERS_Projekat/Program.cs
+++ ERS_Projekat/ERS_Projekat/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERS_Projekat.Consumer;
+using ERS_Projekat.DistributionCentar;
+using ERS_Projekat.SolarPanelsAndWindGenerators;
+
+namespace Projekat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ERS_Projekat.Consumer.Consumer consumer = new ERS_Projekat.Consumer.Consumer();
+
+            SolarPanel solarPanel1 = new SolarPanel("solarPanel1");
+            SolarPanel solarPanel2 = new SolarPanel("solarPanel2");
+            WindGenerator windGen1 = new WindGenerator("windGen1");
+            WindGenerator windGen2 = new WindGenerator("windGen2");
+            DistributiveCenter distributionCenter = new DistributiveCenter(solarPanel1, windGen1);
+
+
+            Device televizor = new Device("Televizor", 4);
+            Device ves_masina = new Device("Ves masina", 2);
+            Device racunar = new Device("Racunar", 3);
+            Device grejalica = new Device("Grejalica", 1);
+            consumer.AddDevices(televizor);
+            consumer.AddDevices(ves_masina);
+            consumer.AddDevices(racunar);
+            consumer.AddDevices(grejalica);
+            consumer.UI(distributionCenter);
+
+        }
+    }
+}
--- ERS_Projekat/ERS_Projekat/obj/Debug/net8.0/ERS_Projekat.AssemblyInfo.cs
+++ ERS_Projekat/ERS_Projekat/obj/Debug/net8.0/ERS_Projekat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERS_Projekat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994f18a9cf703d0fefd515d84e2c6c60bc92950f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERS_Projekat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERS_Projekat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
